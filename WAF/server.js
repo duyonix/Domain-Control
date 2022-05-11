74f0103f43f0ee064586e8094bdff8f57b417f92
binh-nguyen49-jh <binh.nguyen@journeyh.io>
2022-05-11 20:48:33 +0700

    add WAF content

---
+++ WAF/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+
+const Waf = require('mini-waf/wafbase');
+const wafrules = require('mini-waf/wafrules');
+
+const customSetting = { ...wafrules.DefaultSettings };
+customSetting.Rules.push({
+  // DoS rule.
+  Dacls: [],
+  Filters: [
+    {
+      NetworkLayers:
+        Waf.WAF_NETWORK_LAYER.PROTOCOL_IPV4 |
+        Waf.WAF_NETWORK_LAYER.PROTOCOL_IPV6,
+      MatchTypes: Waf.WAF_MATCH_TYPE.MATCH_ATTEMPTS,
+      ManageType: Waf.WAF_MANAGE_TYPE.BLOCK,
+      Directions: Waf.WAF_RULE_DIRECTION.INBOUND,
+
+      Attempts: {
+        MaxAttempts: 100,
+        RenewAttemptsInterval: 600,
+      },
+
+      Description: 'Possible DoS attack.',
+    },
+  ],
+});
+app.use(Waf.WafMiddleware(wafrules.DefaultSettings));
+
+//Create your routes in your way!
+app.use((req, res) => {
+  //Do your work in anywhere.
+  res.send('Some data...');
+  res.end();
+});
+
+app.get('/dos', (req, res) => {
+  res.send({
+    time: new Date().valueOf(),
+    memory: process.memoryUsage(),
+  });
+});
+
+app.listen(3000, function () {
+  console.log('Running server on port 3000!');
+});
